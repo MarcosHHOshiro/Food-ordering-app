7377d191ab5d9cb051ff953bb719581afd75db6d
Marcos <marcos.2h.oshiro@gmail.com>
2025-11-26 20:03:07 -0400

    react router setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
-import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import AppRoutes from './AppRoutes.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <App />
+      <AppRoutes />
     </Router>
   </StrictMode>,
 )
