2c459bfa3682950524142eac54d5bc9c26b3c800
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-11-28 16:12:20 -0400

    User Model

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,9 +2,18 @@ import hero from "../assets/hero.png"
 
 const Hero = () => {
     return (
-        <div>
-            <img src={hero} className="w-full max-h-[600px] object-cover" />
-        </div>
+        <>
+            <div className="flex flex-col">
+                <img src={hero} className="w-full max-h-[600px] object-cover" />
+            </div>
+            <div className="bg-white rounded-lg shadow-md py-8 flex-col gap-5 text-center -mt-16 inline mx-auto p-5">
+                <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+                    Tuck into a takeaway today!
+                </h1>
+                <span className="text-xl">Food is just a click away!</span>
+            </div>
+        </>
+
     )
 }
 
