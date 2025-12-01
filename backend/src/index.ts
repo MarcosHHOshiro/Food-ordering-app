567d5a7b2e76dcc3fb7db16134128533d2cbf1ef
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-12-01 16:02:32 -0400

    Create user on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import myUserRoute from './routes/MyUserRoutes'
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING || '', {})
     .then(() => {
@@ -15,9 +16,8 @@ const app = express();
 app.use(express.json())
 app.use(cors());
 
-app.get("/test", (req, res) => {
-    res.send("Backend is working!");
-});
+// /api/user/user
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
     console.log("Server is running on port 7000");
