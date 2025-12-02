33b00181086bca495ebc349061dfad28116c9baf
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-12-02 15:08:28 -0400

    Validate Access Token

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,5 +1,4 @@
-import { useCreateMyUser } from "@/api/MyUserApi"
-import { Auth0Provider, useAuth0, User, type AppState } from "@auth0/auth0-react"
+import { Auth0Provider } from "@auth0/auth0-react"
 import { useNavigate } from "react-router-dom"
 
 type Props = {
@@ -8,15 +7,17 @@ type Props = {
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
     const navigate = useNavigate()
+
     const domain = import.meta.env.VITE_AUTH0_DOMAIN
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE
 
-    if (!domain || !clientId || !redirectUri) {
+    if (!domain || !clientId || !redirectUri || !audience) {
         throw new Error("Missing Auth0 environment variables")
     }
 
-    const onRedirectCallback = async (appState?: AppState, user?: User) => {
+    const onRedirectCallback = async () => {
 
         navigate("/auth-callback")
     }
@@ -25,7 +26,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
         <Auth0Provider
             domain={domain}
             clientId={clientId}
-            authorizationParams={{ redirect_uri: redirectUri }}
+            authorizationParams={{
+                redirect_uri: redirectUri,
+                audience,
+            }}
             onRedirectCallback={onRedirectCallback}>
             {children}
         </Auth0Provider>
