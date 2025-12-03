0ab4a2033f90c603117875c7ee28df4bb8a18fdd
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-12-03 14:27:08 -0400

    Toast notifications

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -4,9 +4,8 @@ import UserProfileForm from "@/forms/user-profile-form/UserProfileForm"
 const UserProfilePage = () => {
     const { updateUser, isLoading } = useUpdateMyUser()
 
-    return (
-        <UserProfileForm onSave={updateUser} isLoading={isLoading} />
-    )
+    return <UserProfileForm onSave={updateUser} isLoading={isLoading} />
+    
 }
 
 export default UserProfilePage
